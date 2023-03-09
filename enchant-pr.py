f2bcf7301fae8657a6367440edfed805e80f975e
zhixi <106742614+leezhixi@users.noreply.github.com>
2023-03-09 17:43:24 +0800

    Create enchant-pr.py
    made the easy one first...

---
+++ enchant-pr.py
@@ -0,0 +1,11 @@
+# Be sure to keep an eye on ur discord every now and then
+# just try to not get into jail xdd
+
+import pyautogui as pag
+import time
+import random
+
+while True:
+    pag.write('rpg transmute s')
+    pag.enter('enter')
+    time.sleep(random.randint(1,2))
