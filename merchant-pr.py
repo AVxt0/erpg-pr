f4463e59355cd45a2fa6c27827071e7db97621e2
zhixi <106742614+leezhixi@users.noreply.github.com>
2023-03-09 17:59:42 +0800

    Create merchant-pr.py

---
+++ merchant-pr.py
@@ -0,0 +1,48 @@
+
+# Be sure to keep an eye on ur discord every now and then
+# just try to not get into jail xdd
+
+# side note: dismantles everything in your inventory
+# sells mob drops too
+# read this before !!!!!!!!!
+
+import pyautogui as pag
+import time
+import random
+
+while True:
+    pag.write('rpg dismantle epic fish all')
+    pag.enter('enter')
+    time.sleep(random.randint(1,2))
+    pag.write('rpg dismantle golden fish all')
+    pag.enter('enter')
+    time.sleep(random.randint(1,2))
+    pag.write('rpg dismantle trade a all')
+    pag.enter('enter')
+    time.sleep(random.randint(1,2))
+    pag.write('rpg dismantle banana all')
+    pag.enter('enter')
+    time.sleep(random.randint(1,2))
+    pag.write('rpg dismantle trade c all')
+    pag.enter('enter')
+    time.sleep(random.randint(1,2))
+    pag.write('rpg dismantle trade e all')
+    pag.enter('enter')
+    time.sleep(random.randint(1,2))
+    pag.write('rpg dismantle ultra log all')
+    pag.enter('enter')
+    time.sleep(random.randint(1,2))
+    pag.write('rpg dismantle hyper log all')
+    pag.enter('enter')
+    time.sleep(random.randint(1,2))
+    pag.write('rpg dismantle mega log all')
+    pag.enter('enter')
+    time.sleep(random.randint(1,2))
+    pag.write('rpg dismantle super log all')
+    pag.enter('enter')
+    time.sleep(random.randint(1,2))
+    pag.write('rpg dismantle epic log all')
+    pag.enter('enter')
+    time.sleep(random.randint(1,2))
+    pag.write('rpg sell wooden log all')
+    pag.enter('enter')
