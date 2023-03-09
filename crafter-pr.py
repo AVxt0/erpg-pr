c11e12c64c9fc8e01e79860a38d61af8b3ada61f
zhixi <106742614+leezhixi@users.noreply.github.com>
2023-03-09 17:55:38 +0800

    Create crafter-pr.py

---
+++ crafter-pr.py
@@ -0,0 +1,17 @@
+
+# Be sure to keep an eye on ur discord every now and then
+# just try to not get into jail xdd
+
+# crafts in batches of 1000 epic logs
+
+import pyautogui as pag
+import time
+import random
+
+while True:
+    pag.write('rpg craft epic log 1000')
+    pag.enter('enter')
+    time.sleep(random.randint(1,2))
+    pag.write('rpg dismantle epic log all')
+    pag.enter('enter')
+    time.sleep(random.randint(1,2))
