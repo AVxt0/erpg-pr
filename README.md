8887d1c587c1a3e3f7c9eb436db7e8a3501d475b
AVxt0 <zhixi0819@gmail.com>
2023-03-13 14:42:58 +0800

    merchant-pr massive script update

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 using python to make erpg-pr grind easier :D
 
 keep in mind u need pyautogui to make the thing work...
+
+merchant pr script is just to sell everything in ur inventory
--- merchant-pr.py
+++ merchant-pr.py
@@ -4,6 +4,9 @@
 
 # side note: dismantles everything in your inventory
 # sells mob drops too
+# wooden logs are sold in batches of 50k because more money lol
+# if you want to do it all at once, just stop the script once it finishes
+# selling mod drops
 # read this before !!!!!!!!!
 
 # to stop the script, hover mouse to top left corner of your screen
@@ -14,6 +17,31 @@ import random
 
 pag.FAILSAFE = True
 time.sleep(1.5)
+
+# opens lootboxes
+
+pag.write('rpg open ed all')
+pag.keyDown('enter')
+pag.keyUp('enter')
+time.sleep(random.randint(1,2))
+pag.write('rpg open ep all')
+pag.keyDown('enter')
+pag.keyUp('enter')
+time.sleep(random.randint(1,2))
+pag.write('rpg open rare all')
+pag.keyDown('enter')
+pag.keyUp('enter')
+time.sleep(random.randint(1,2))
+pag.write('rpg open u all')
+pag.keyDown('enter')
+pag.keyUp('enter')
+time.sleep(random.randint(1,2))
+pag.write('rpg open c all')
+pag.keyDown('enter')
+pag.keyUp('enter')
+time.sleep(random.randint(1,2))
+
+# mats
 
 pag.write('rpg dismantle epic fish all')
 pag.keyDown('enter')
@@ -59,6 +87,68 @@ pag.write('rpg dismantle epic log all')
 pag.keyDown('enter')
 pag.keyUp('enter')
 time.sleep(random.randint(1,2))
+
+# mob drops
+
+pag.write('rpg sell wolf skin all')
+pag.keyDown('enter')
+pag.keyUp('enter')
+time.sleep(random.randint(1,2))
+pag.write('rpg sell zombie eye all')
+pag.keyDown('enter')
+pag.keyUp('enter')
+time.sleep(random.randint(1,2))
+pag.write('rpg sell unicorn horn all')
+pag.keyDown('enter')
+pag.keyUp('enter')
+time.sleep(random.randint(1,2))
+pag.write('rpg sell mermaid hair all')
+pag.keyDown('enter')
+pag.keyUp('enter')
+time.sleep(random.randint(1,2))
+pag.write('rpg sell chip all')
+pag.keyDown('enter')
+pag.keyUp('enter')
+time.sleep(random.randint(1,2))
+pag.write('rpg sell dragon scale all')
+pag.keyDown('enter')
+pag.keyUp('enter')
+time.sleep(random.randint(1,2))
+
+# farm items
+
+pag.write('rpg sell bread all')
+pag.keyDown('enter')
+pag.keyUp('enter')
+time.sleep(random.randint(1,2))
+pag.write('rpg sell carrot all')
+pag.keyDown('enter')
+pag.keyUp('enter')
+time.sleep(random.randint(1,2))
+pag.write('rpg sellpotato all')
+pag.keyDown('enter')
+pag.keyUp('enter')
+time.sleep(random.randint(1,2))
+pag.write('rpg sell seed all')
+pag.keyDown('enter')
+pag.keyUp('enter')
+time.sleep(random.randint(1,2))
+pag.write('rpg sell bread seed all')
+pag.keyDown('enter')
+pag.keyUp('enter')
+time.sleep(random.randint(1,2))
+pag.write('rpg sell carrot seed all')
+pag.keyDown('enter')
+pag.keyUp('enter')
+time.sleep(random.randint(1,2))
+pag.write('rpg sell potato seed all')
+pag.keyDown('enter')
+pag.keyUp('enter')
+time.sleep(random.randint(1,2))
+
+# misc items
+
+pag.write('rpg sell life potion all')
 
 while True:
     pag.write('rpg sell wooden log 50k')
