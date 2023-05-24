01e5bcef75665e96de51c7c4f65ede8f600b2d85
zhixi <106742614+erpg-scripts@users.noreply.github.com>
2023-05-24 18:21:50 +0800

    .?l

--- calc/calc.py
+++ calc/calc.py
@@ -7,5 +7,13 @@ pag.FAILSAFE = True
 
 print('----- EPIC RPG Related Calculator -----\n')
 print('1. A10 Mats Calculator')
-print('2. Crafting calculator')
-calc_type = 
+print('2. Crafting calculator\n')
+
+while True:
+    calc_type = input('Enter your choice (1/2):  ')
+    print('')
+
+    if calc_type == '1':
+        area = input('Area:  ')
+        if area == '3':
+            
