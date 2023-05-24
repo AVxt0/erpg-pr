8b1c39c480bb146227ab34b3ec2de591efa60bfb
zhixi <zhixi0819@gmail.com>
2023-05-24 18:13:04 +0800

    creating calc.py

---
+++ calc/calc.py
@@ -0,0 +1,11 @@
+import pyautogui as pag
+import time
+import random
+import erpg
+
+pag.FAILSAFE = True
+
+print('----- EPIC RPG Related Calculator -----\n')
+print('1. A10 Mats Calculator')
+print('2. Crafting calculator')
+calc_type = 
